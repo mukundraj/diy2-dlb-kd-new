eada877eabb450f11e8b2a91f784fff9e3853924
mraj <marj@anl.gov>
2019-08-21 08:47:10 -0500

    Baseline scripts updated

---
+++ results/baseline/run_nek_bebop.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=baseline
+#SBATCH --account=pedal
+#SBATCH --partition=knlall
+#SBATCH --constraint knl,quad,cache
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=64
+#SBATCH --output=<my_job_name>.out
+#SBATCH --error=<my_job_name>.error
+#SBATCH --mail-user=mraj@lcrc.anl.gov
+#SBATCH --time=0:015:00
+
+srun -n 2 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_02
+srun -n 4 ./sync_nek configs_48/config.nek4.xml 2> 48/nek_04
+srun -n 8 ./sync_nek configs_48/config.nek8.xml 2> 48/nek_08
+srun -n 16 ./sync_nek configs_48/config.nek16.xml 2> 48/nek_16
+srun -n 32 ./sync_nek configs_48/config.nek32.xml 2> 48/nek_32
+srun -n 64 ./sync_nek configs_48/config.nek64.xml 2> 48/nek_16
+srun -n 128 ./sync_nek configs_48/config.nek128.xml 2> 48/nek_32
+srun -n 256 ./sync_nek configs_48/config.nek256.xml 2> 48/nek_16
+srun -n 512 ./sync_nek configs_48/config.nek512.xml 2> 48/nek_32
+
+srun -n 2 ./sync_nek configs_96/config.nek2.xml 2> 96/nek_02
+srun -n 4 ./sync_nek configs_96/config.nek4.xml 2> 96/nek_04
+srun -n 8 ./sync_nek configs_96/config.nek8.xml 2> 96/nek_08
+srun -n 16 ./sync_nek configs_96/config.nek16.xml 2> 96/nek_16
+srun -n 32 ./sync_nek configs_96/config.nek32.xml 2> 96/nek_32
+srun -n 64 ./sync_nek configs_96/config.nek64.xml 2> 96/nek_16
+srun -n 128 ./sync_nek configs_96/config.nek128.xml 2> 96/nek_32
+srun -n 256 ./sync_nek configs_96/config.nek256.xml 2> 96/nek_16
+srun -n 512 ./sync_nek configs_96/config.nek512.xml 2> 96/nek_32
+
+srun -n 2 ./sync_nek configs_384/config.nek2.xml 2> 128/nek_02
+srun -n 4 ./sync_nek configs_384/config.nek4.xml 2> 128/nek_04
+srun -n 8 ./sync_nek configs_384/config.nek8.xml 2> 128/nek_08
+srun -n 16 ./sync_nek configs_384/config.nek16.xml 2> 128/nek_16
+srun -n 32 ./sync_nek configs_384/config.nek32.xml 2> 128/nek_32
+srun -n 64 ./sync_nek configs_384/config.nek64.xml 2> 128/nek_16
+srun -n 128 ./sync_nek configs_384/config.nek128.xml 2> 128/nek_32
+srun -n 256 ./sync_nek configs_384/config.nek256.xml 2> 128/nek_16
+srun -n 512 ./sync_nek configs_384/config.nek512.xml 2> 128/nek_32
