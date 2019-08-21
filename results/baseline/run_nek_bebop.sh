49e70ba139e74ec3078ed9b489799276f7604ac0
mraj <marj@anl.gov>
2019-08-21 14:40:31 -0500

    minor

--- results/baseline/run_nek_bebop.sh
+++ results/baseline/run_nek_bebop.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=baseline.%j.%N.out
 #SBATCH --error=baseline.%j.%N.error
 #SBATCH --mail-user=mraj@lcrc.anl.gov
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 
 
 srun -n 1 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_01
