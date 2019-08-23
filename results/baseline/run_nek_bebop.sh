65f3c86dc433f3b3eccac4f4dccddccaf4866aea
mraj <marj@anl.gov>
2019-08-23 08:26:13 -0500

    minor

--- results/baseline/run_nek_bebop.sh
+++ results/baseline/run_nek_bebop.sh
@@ -9,38 +9,38 @@
 #SBATCH --output=baseline.%j.%N.out
 #SBATCH --error=baseline.%j.%N.error
 #SBATCH --mail-user=mraj@lcrc.anl.gov
-#SBATCH --time=03:00:00
+#SBATCH --time=02:00:00
 
 
-srun -n 1 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_01
-srun -n 2 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_02
-srun -n 4 ./sync_nek configs_48/config.nek4.xml 2> 48/nek_04
-srun -n 8 ./sync_nek configs_48/config.nek8.xml 2> 48/nek_08
-srun -n 16 ./sync_nek configs_48/config.nek16.xml 2> 48/nek_16
-srun -n 32 ./sync_nek configs_48/config.nek32.xml 2> 48/nek_32
-srun -n 64 ./sync_nek configs_48/config.nek64.xml 2> 48/nek_64
-# srun -n 128 ./sync_nek configs_48/config.nek128.xml 2> 48/nek_128
-# srun -n 256 ./sync_nek configs_48/config.nek256.xml 2> 48/nek_256
-# srun -n 512 ./sync_nek configs_48/config.nek512.xml 2> 48/nek_512
+# srun -n 1 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_01
+# srun -n 2 ./sync_nek configs_48/config.nek2.xml 2> 48/nek_02
+# srun -n 4 ./sync_nek configs_48/config.nek4.xml 2> 48/nek_04
+# srun -n 8 ./sync_nek configs_48/config.nek8.xml 2> 48/nek_08
+# srun -n 16 ./sync_nek configs_48/config.nek16.xml 2> 48/nek_16
+# srun -n 32 ./sync_nek configs_48/config.nek32.xml 2> 48/nek_32
+# srun -n 64 ./sync_nek configs_48/config.nek64.xml 2> 48/nek_64
+srun -n 128 ./sync_nek configs_48/config.nek128.xml 2> 48/nek_128
+srun -n 256 ./sync_nek configs_48/config.nek256.xml 2> 48/nek_256
+srun -n 512 ./sync_nek configs_48/config.nek512.xml 2> 48/nek_512
 
-srun -n 1 ./sync_nek configs_96/config.nek2.xml 2> 96/nek_01
-srun -n 2 ./sync_nek configs_96/config.nek2.xml 2> 96/nek_02
-srun -n 4 ./sync_nek configs_96/config.nek4.xml 2> 96/nek_04
-srun -n 8 ./sync_nek configs_96/config.nek8.xml 2> 96/nek_08
-srun -n 16 ./sync_nek configs_96/config.nek16.xml 2> 96/nek_16
-srun -n 32 ./sync_nek configs_96/config.nek32.xml 2> 96/nek_32
-srun -n 64 ./sync_nek configs_96/config.nek64.xml 2> 96/nek_64
-# srun -n 128 ./sync_nek configs_96/config.nek128.xml 2> 96/nek_128
-# srun -n 256 ./sync_nek configs_96/config.nek256.xml 2> 96/nek_256
-# srun -n 512 ./sync_nek configs_96/config.nek512.xml 2> 96/nek_512
+# srun -n 1 ./sync_nek configs_96/config.nek2.xml 2> 96/nek_01
+# srun -n 2 ./sync_nek configs_96/config.nek2.xml 2> 96/nek_02
+# srun -n 4 ./sync_nek configs_96/config.nek4.xml 2> 96/nek_04
+# srun -n 8 ./sync_nek configs_96/config.nek8.xml 2> 96/nek_08
+# srun -n 16 ./sync_nek configs_96/config.nek16.xml 2> 96/nek_16
+# srun -n 32 ./sync_nek configs_96/config.nek32.xml 2> 96/nek_32
+# srun -n 64 ./sync_nek configs_96/config.nek64.xml 2> 96/nek_64
+srun -n 128 ./sync_nek configs_96/config.nek128.xml 2> 96/nek_128
+srun -n 256 ./sync_nek configs_96/config.nek256.xml 2> 96/nek_256
+srun -n 512 ./sync_nek configs_96/config.nek512.xml 2> 96/nek_512
 
-srun -n 1 ./sync_nek configs_384/config.nek2.xml 2> 384/nek_01
-srun -n 2 ./sync_nek configs_384/config.nek2.xml 2> 384/nek_02
-srun -n 4 ./sync_nek configs_384/config.nek4.xml 2> 384/nek_04
-srun -n 8 ./sync_nek configs_384/config.nek8.xml 2> 384/nek_08
-srun -n 16 ./sync_nek configs_384/config.nek16.xml 2> 384/nek_16
-srun -n 32 ./sync_nek configs_384/config.nek32.xml 2> 384/nek_32
-srun -n 64 ./sync_nek configs_384/config.nek64.xml 2> 384/nek_64
-# srun -n 128 ./sync_nek configs_384/config.nek128.xml 2> 128/nek_128
-# srun -n 256 ./sync_nek configs_384/config.nek256.xml 2> 128/nek_256
-# srun -n 512 ./sync_nek configs_384/config.nek512.xml 2> 128/nek_512
+# srun -n 1 ./sync_nek configs_384/config.nek2.xml 2> 384/nek_01
+# srun -n 2 ./sync_nek configs_384/config.nek2.xml 2> 384/nek_02
+# srun -n 4 ./sync_nek configs_384/config.nek4.xml 2> 384/nek_04
+# srun -n 8 ./sync_nek configs_384/config.nek8.xml 2> 384/nek_08
+# srun -n 16 ./sync_nek configs_384/config.nek16.xml 2> 384/nek_16
+# srun -n 32 ./sync_nek configs_384/config.nek32.xml 2> 384/nek_32
+# srun -n 64 ./sync_nek configs_384/config.nek64.xml 2> 384/nek_64
+srun -n 128 ./sync_nek configs_384/config.nek128.xml 2> 128/nek_128
+srun -n 256 ./sync_nek configs_384/config.nek256.xml 2> 128/nek_256
+srun -n 512 ./sync_nek configs_384/config.nek512.xml 2> 128/nek_512
